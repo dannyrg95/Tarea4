cd374523f380339b1131fe1f1ad63d7bc9ed23cf
danny <danny@DANNY-ASUS>
2022-04-17 09:25:49 -0600

    Tarea 4 // Se realiza Tarea sobre Jasper Reports

---
+++ src/main/java/com/tienda_M/controller/ClienteController.java
@@ -0,0 +1,82 @@
+package com.tienda_M.controller;
+
+import com.tienda_M.domain.Cliente;
+import com.tienda_M.service.ClienteReportService;
+import com.tienda_M.service.ClienteService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+@Controller
+@Slf4j
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private ClienteReportService clienteReportService;
+
+    @GetMapping("/cliente/listado")
+    public String inicio(Model model) {
+        var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
+        }
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
+        return "/cliente/listado";
+    }
+
+    @GetMapping("/cliente/nuevo")
+    public String nuevoCliente(Cliente cliente) {
+        return "/cliente/modifica";
+    }
+
+    @PostMapping("/cliente/guardar")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/cliente/listado";
+    }
+
+    @GetMapping("/cliente/modificar/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        var respuesta = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", respuesta);
+        return "/cliente/modifica";
+    }
+
+    @GetMapping("/cliente/eliminar/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/cliente/listado";
+    }
+
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
